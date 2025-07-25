3ee46493f7592479e52dc225ca7b6de2e610bd8d
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-07-25 08:36:08 +0100

    Initial commit

---
+++ src/main/java/ivancroce/u2_w2_d5_corporate_agency/U2W2D5CorporateAgencyApplication.java
@@ -0,0 +1,13 @@
+package ivancroce.u2_w2_d5_corporate_agency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W2D5CorporateAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W2D5CorporateAgencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ivancroce/u2_w2_d5_corporate_agency/U2W2D5CorporateAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package ivancroce.u2_w2_d5_corporate_agency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W2D5CorporateAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
